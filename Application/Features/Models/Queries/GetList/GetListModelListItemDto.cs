4e15edb91e0739f1169e819ef1cc9aa08019f674
kadirkaradag <kadirkaradag0132@gmail.com>
2024-01-24 21:04:14 +0300

    include substructure

---
+++ Application/Features/Models/Queries/GetList/GetListModelListItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Features.Models.Queries.GetList;
+
+public class GetListModelListItemDto
+{
+    public Guid Id { get; set; }
+    public string BrandName { get; set; }  //bu bilginin joinlenerek diğer tablodan gelmesini bekliyorum
+    public string FuelName { get; set; }  //bu bilginin joinlenerek diğer tablodan gelmesini bekliyorum
+    public string TransmissionName { get; set; }  //bu bilginin joinlenerek diğer tablodan gelmesini bekliyorum
+    public string Name { get; set; }
+    public decimal DailyPrice { get; set; }
+    public string ImageUrl { get; set; }
+}
