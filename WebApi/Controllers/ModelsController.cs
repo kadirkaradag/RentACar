90be7823aea0be50bb48b5b9f64c62731a4e4f1e
kadirkaradag <kadirkaradag0132@gmail.com>
2024-01-26 21:19:24 +0300

    dynamic search substructure used on model

--- WebApi/Controllers/ModelsController.cs
+++ WebApi/Controllers/ModelsController.cs
@@ -1,8 +1,8 @@
-﻿using Application.Features.Brands.Queries.GetList;
-using Application.Features.Models.Queries.GetList;
+﻿using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using Core.Application.Requests;
 using Core.Application.Responses;
-using MediatR;
+using Core.Persistence.Dynamic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
@@ -16,6 +16,14 @@ namespace WebApi.Controllers
         {
             GetListModelQuery getListModelQuery = new() { PageRequest = pageRequest };
             GetListResponse<GetListModelListItemDto> response = await Mediator.Send(getListModelQuery);
+            return Ok(response);
+        }
+
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] DynamicQuery? dynamicQuery=null)
+        {
+            GetListByDynamicModelQuery getListByDynamicModelQuery = new() { PageRequest = pageRequest,DynamicQuery=dynamicQuery };
+            GetListResponse<GetListByDynamicModelListItemDto> response = await Mediator.Send(getListByDynamicModelQuery);
             return Ok(response);
         }
 
