b3f73d5f48cefb3a69118ed5f38b3de336a70e7b
kadirkaradag <kadirkaradag0132@gmail.com>
2024-02-26 21:46:45 +0300

    redis cache implementation.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -10,7 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 
-builder.Services.AddDistributedMemoryCache(); //inMemory
+//builder.Services.AddDistributedMemoryCache(); //inMemory cache
+
+//redis icin microsoft un StackExchangeRedis paketini kurduk webApi projesine
+//docker ý baslattým, cmd ye; docker run --name narch-redis -p 6379:6379 -d redis
+//yazdýk redis i kurdu, sonra redis insigth tool unu indirdik sitesinden. o kendi docker ýn redisini gördü ve projeyi calýstýrýp request attýgýmýzda  redis insigth üzerinde requesti gördük.
+builder.Services.AddStackExchangeRedisCache(opt => opt.Configuration = "localhost:6379");  
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
